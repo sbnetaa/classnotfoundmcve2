3d21c6674c3843553aa128bdd5edb7ed6f3ab4c8
Dmitriy T. <sbnet@bk.ru>
2024-09-01 16:45:16 +0300

    first commit

---
+++ src/test/java/ru/terentyev/ClassNotFoundMCVE2/ClassNotFoundMcve2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.terentyev.ClassNotFoundMCVE2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassNotFoundMcve2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
